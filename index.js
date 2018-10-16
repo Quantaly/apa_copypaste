2a7b2bf9e925409cdac9e10b988ae11425613ef0
Kai Page <kaibug@gmail.com>
2018-10-16 09:08:05 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+let author = document.getElementById("author");
+let date = document.getElementById("date");
+let title = document.getElementById("title");
+let retrieved = document.getElementById("retrieved");
+let url = document.getElementById("url");
+let output = document.getElementById("output");
+let selectAll = document.getElementById("selectall");
+
+function updateOutput() {
+    let ret = author.value;
+	ret += ". (";
+	ret += date.value;
+	ret += "). ";
+	ret += title.value;
+	ret += " Retrieved ";
+	ret += retrieved.value;
+	ret += ", from ";
+	ret += url.value;
+	ret += ".";
+	output.value = ret;
+}
+
+function selectAllF() {
+	output.selectionStart = 0;
+	output.selectionEnd = output.value.length;
+	output.focus();
+}
+
+author.addEventListener("input", updateOutput);
+date.addEventListener("input", updateOutput);
+title.addEventListener("input", updateOutput);
+retrieved.addEventListener("input", updateOutput);
+url.addEventListener("input", updateOutput);
+output.addEventListener("input", updateOutput);
+
+selectAll.addEventListener("click", selectAllF);
